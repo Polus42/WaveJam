50b7218db63c31be7bd06df9a3179786e7c1b3c2
Armel <armel.perhirin@orange.fr>
2017-01-22 00:09:59 +0100

    Vide + Checkpoints

---
+++ Assets/Scripts/Armel/VideBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideBehaviour : MonoBehaviour {
+
+    private GlobalBehaviour levelManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        levelManager = FindObjectOfType<GlobalBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            levelManager.RespawnPlayer();
+        }
+    }
+}
